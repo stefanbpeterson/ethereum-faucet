deef3658bb5d0cad3dc9a7f9b35a94b34b0cd9d3
stefanbpeterson <stefanbpeterson@gmail.com>
2022-01-30 21:00:57 -0700

    updating truffle compiler version and adding receive function to faucet contract

--- truffle-config.js
+++ truffle-config.js
@@ -4,13 +4,13 @@ module.exports = {
   networks: {
     development: {
      host: "127.0.0.1",     // Localhost (default: none)
-     port: 8545,            // Standard Ethereum port (default: none)
+     port: 7545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
     },
   },
   compilers: {
     solc: {
-      version: "0.5.1",
+      version: "0.8.4",
     }
   },
 };
