62beae7967678cfec9fed4412265b844a8e299d0
stefanbpeterson <stefanbpeterson@gmail.com>
2022-02-18 15:20:18 -0700

    adding a modifier and withdraw from contract function

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -2,7 +2,45 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
+
+    uint public numOfFunders;
+    mapping(address => bool) public funders;
+    mapping(uint => address) public lutFunders;
+
     receive() external payable {
-        
+
+    }
+
+    modifier mustBeBelow(uint withdrawAmount) {
+        require(withdrawAmount <= 1000000000000000000, "Cannot withdraw more than 1 ether.");
+        _;
+    }
+
+    function addFunds() external payable {
+        address funder = msg.sender;
+
+        if(!funders[funder]) {
+            uint index = numOfFunders++;
+            funders[funder] = true;
+            lutFunders[index] = funder;
+        }
+    }
+
+    function withdrawFunds(uint withdrawAmount) external mustBeBelow(withdrawAmount) {
+        payable(msg.sender).transfer(withdrawAmount);
+    }
+
+    function getAllFunders() external view returns(address[] memory) {
+        address[] memory _funders = new address[] (numOfFunders);
+
+        for(uint i = 0; i < numOfFunders; i++) {
+            _funders[i] = lutFunders[i];
+        }
+
+        return _funders;
+    }
+
+    function getFunderAtIndex(uint index) external view returns(address) {
+        return lutFunders[index];
     }
 }
