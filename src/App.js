43b2fb1b6ab8dbbd4b881144682db95bab1ffc4b
stefanbpeterson <stefanbpeterson@gmail.com>
2022-02-24 08:04:01 -0700

    adding enable ethereum button and connecting metamask to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import react from "react";
 import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const loadProvider = async() => {
+      console.log(window.web3)
+      console.log(window.ethereum)
+    }
+
+    loadProvider()
+  }, [])
+  
+
   return (
     <>
       <div className="faucet-wrapper">
@@ -8,6 +20,10 @@ function App() {
           <div className="balance-view is-size-2">
             Current Balance: <strong>10</strong>
           </div>
+          <button className="btn mr-2" onClick={ async() => {
+            const accounts = await window.ethereum.request({method: "eth_requestAccounts"})
+            console.log(accounts)
+          }}>Enable Ethereum</button>
           <button className="btn mr-2">Donate</button>
           <button className="btn">Withdraw</button>
         </div>
